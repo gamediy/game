e5493fd6df3329eb6720bdc1ed0fef5484be964b
abri <abri@gmail.com>
2023-10-24 13:50:47 +0800

    game sync

--- consts/ws_consts/ws.go
+++ consts/ws_consts/ws.go
@@ -1,14 +1,16 @@
 package ws_consts
 
 const (
-	Error     = "/error"
-	Heartbeat = "/heartbeat"
-	Login     = "/user/login"
-	Join      = "/user/join"
-	Quit      = "/user/quit"
-	Wallet    = "/user/wallet"
-	Enter     = "/game/enter"
+	Error        = "/error"
+	Heartbeat    = "/heartbeat"
+	Login        = "/user/login"
+	Join         = "/user/join"
+	Quit         = "/user/quit"
+	Wallet       = "/user/wallet"
+	MailBoxTotal = "/user/mail_box_total" //未读数量
+	MailBoxPlus  = "/user/mail_box_plus"  //未读数量加1
 
 	SlotSpin     = "/slot/spin"
 	SlotCheckWon = "/slot/checkWon"
+	Enter        = "/game/enter"
 )
