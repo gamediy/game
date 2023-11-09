49f1224e42a5cf6eb5274d1cd754356c6d1fb3d9
abri <abri@gmail.com>
2023-11-09 16:04:52 +0800

    socket

--- app/gateway/main.go
+++ app/gateway/main.go
@@ -1,11 +1,37 @@
 package main
 
 import (
-	"github.com/gogf/gf/v2/os/gctx"
-
+	"fmt"
 	"game/app/gateway/internal/cmd"
+	"github.com/gogf/gf/v2/os/gctx"
+	"os"
+	"runtime/pprof"
 )
 
 func main() {
 	cmd.Main.Run(gctx.GetInitCtx())
+	cpuProfile, err := os.Create("./pprof/cpu_profile")
+	if err != nil {
+		fmt.Printf("创建文件失败:%s", err.Error())
+		return
+	}
+	defer cpuProfile.Close()
+
+	memProfile, err := os.Create("./pprof/mem_profile")
+	if err != nil {
+		fmt.Printf("创建文件失败:%s", err.Error())
+		return
+	}
+	defer memProfile.Close()
+	//采集CPU信息
+	pprof.StartCPUProfile(cpuProfile)
+	defer pprof.StopCPUProfile()
+
+	//采集内存信息
+	pprof.WriteHeapProfile(memProfile)
+
+	for i := 0; i < 100; i++ {
+		fmt.Println("pprof 工具型测试")
+	}
+
 }
